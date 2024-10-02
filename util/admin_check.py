0ccbe51c8bb3fb5f0a493691dcfbf9fa44283906
NattyanTV <73095116+nattyan-tv@users.noreply.github.com>
2024-10-02 14:08:35 +0000

    Revert "change: admin_checkをより簡潔なコードに"
    This reverts commit 943452e55c59bfa6a7119da77613f418d6c81092.

--- util/admin_check.py
+++ util/admin_check.py
@@ -7,13 +7,17 @@ from util import n_fc
 # ユーザーがそのサーバーで管理者権限を持っているか確認する。
 
 
-def admin_check(guild: nextcord.Guild, memb: nextcord.Member) -> bool:
+def admin_check(guild: nextcord.Message.guild, memb: nextcord.Member) -> bool:
+    role_list = []
     if memb.id in n_fc.py_admin:
         return True
     if memb.id == guild.owner_id:
         return True
     for role in memb.roles:
-        if role.permissions.administrator:
+        role_list.append(role.id)
+    for i in range(len(role_list)):
+        role = get(guild.roles, id=role_list[i])
+        if (role.permissions).administrator:
             return True
     return False
 
